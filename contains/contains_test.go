c208d7b2f6377ec4888cfe3aeee177295334ba6c
KyleBanks <kyle@nthgensoftware.com>
2016-07-04 19:34:11 -0400

    Simplified contains test cases

--- contains/contains_test.go
+++ contains/contains_test.go
@@ -4,14 +4,14 @@ import "testing"
 
 func TestInt(t *testing.T) {
 	// Negative cases
-	if contains := Int(0, []int{}); contains {
+	if Int(0, []int{}) {
 		t.Fatal("Expected value not to be in empty slice")
-	} else if contains := Int(0, []int{1, 2, 3}); contains {
+	} else if Int(0, []int{1, 2, 3}) {
 		t.Fatal("Expected value not to be in slice")
 	}
 
 	// Positive Cases
-	if contains := Int(0, []int{1, 2, 3, 0}); !contains {
+	if !Int(0, []int{1, 2, 3, 0}) {
 		t.Fatal("Expected value to be in slice")
 	}
 }
