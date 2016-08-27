09f7135efe3c774238435876e29bb446d2582649
KyleBanks <kyle@nthgensoftware.com>
2016-08-27 10:23:12 -0400

    Contains updates, removed dynamo package, re-added sqlite3 dependency

--- contains/contains.go
+++ contains/contains.go
@@ -2,8 +2,27 @@ package contains
 
 // Int returns true if the slice of ints contains the value provided.
 func Int(val int, arr []int) bool {
+	iarr := make([]interface{}, len(arr))
+	for i, a := range arr {
+		iarr[i] = a
+	}
+
+	return contains(val, iarr)
+}
+
+// Uint returns true if the slice of uints contains the value provided.
+func Uint(val uint, arr []uint) bool {
+	iarr := make([]interface{}, len(arr))
+	for i, a := range arr {
+		iarr[i] = a
+	}
+
+	return contains(val, iarr)
+}
+
+func contains(v interface{}, arr []interface{}) bool {
 	for _, i := range arr {
-		if i == val {
+		if i == v {
 			return true
 		}
 	}
--- contains/contains_test.go
+++ contains/contains_test.go
@@ -15,3 +15,17 @@ func TestInt(t *testing.T) {
 		t.Fatal("Expected value to be in slice")
 	}
 }
+
+func TestUint(t *testing.T) {
+	// Negative cases
+	if Uint(uint(0), []uint{}) {
+		t.Fatal("Expected value not to be in empty slice")
+	} else if Uint(uint(0), []uint{uint(1), uint(2), uint(3)}) {
+		t.Fatal("Expected value not to be in slice")
+	}
+
+	// Positive Cases
+	if !Uint(0, []uint{uint(1), uint(2), uint(3), uint(0)}) {
+		t.Fatal("Expected value to be in slice")
+	}
+}
