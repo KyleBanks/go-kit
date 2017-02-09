a373ad9e70a4e918de9b1c9e09aa5dcb755eaad3
KyleBanks <kyle@nthgensoftware.com>
2017-02-08 20:04:38 -0500

    Ensure golint is available when running sanity script

--- sanity.sh
+++ sanity.sh
@@ -3,6 +3,8 @@ set -e
 
 cd $GOPATH/src/github.com/KyleBanks/go-kit
 
+go get -u github.com/golang/lint/golint
+
 echo "-------------  TEST  -------------"
 go test -cover $@ $(go list ./... | grep -v vendor/)
 
